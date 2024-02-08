76a016395e2ebab9915aaf943c342332f23d7444
AyeMyintSan <ayemyintsan@gmail.com>
2024-02-08 13:46:10 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from './components/button/Button'
+import Navbar from './components/nav/Navbar'
+import Navrr from './components/navrr/Navrr'
+
+function App() {
+  return (
+    <>
+     
+        <Button/>
+        <Navbar/>
+        <Navrr/>
+        
+    
+    </>
+  )
+}
+
+export default App
